99a2a7b704563979fb7f58985420a2b467b523dd
anies1212 <all.for.me1212sna@gmail.com>
2023-02-06 23:35:29 +0900

    Impl WelcomeViewController

--- Tech_Accel_Travel_Book/SceneDelegate.swift
+++ Tech_Accel_Travel_Book/SceneDelegate.swift
@@ -11,11 +11,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-    func scene(_ scene: UIScene,
-               willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard scene is UIWindowScene else {
-                    return
-        }
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = WelcomeViewController()
+        window?.windowScene = windowScene
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {}
