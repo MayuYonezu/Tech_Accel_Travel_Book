ebad5fb7206911d728a6606fb5cb4769e87e4e8b
anies1212 <all.for.me1212sna@gmail.com>
2023-02-12 18:39:53 +0900

    Revert "間違ったいらないコミット！リモートにもあげてしまったコミット！"
    This reverts commit 22a5b2e678a94a5977cccf9cadfaf5248ee8b801.

--- Tech_Accel_Travel_Book/SceneDelegate.swift
+++ Tech_Accel_Travel_Book/SceneDelegate.swift
@@ -23,9 +23,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window?.windowScene = windowScene
     }
 
-
-
-
     func sceneDidDisconnect(_ scene: UIScene) {}
 
     func sceneDidBecomeActive(_ scene: UIScene) {}
